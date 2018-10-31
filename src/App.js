779fbe71842a9d7f717d1f988e1ba6273165c12b
ainokainen <aino.rouvinen@gmail.com>
2018-10-31 08:49:17 +0200

    Navigaatio lisätty

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from "jquery";
+import Popper from "popper.js";
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import {Jumbotron} from 'react-bootstrap';
 import React, { Component } from 'react';
 import './App.css';
 import TehtavaBox from "./components/TehtavaBox";
+import Navigation from './components/Navigation';
 
 class App extends Component {
   render() {
     return (
+      // <Jumbotron>
       <div className="App">
         <header className="App-header">
+        <Navigation/>
           <TehtavaBox/>
         </header>
       </div>
+      // </Jumbotron>
     );
   }
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+/* import classNames from 'classnames';
+import keycode from 'keycode';
+import React, { cloneElement } from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import all from 'prop-types-extra/lib/all';
+import warning from 'warning'; */
+
+/* import {
+  bsClass,
+  bsStyles,
+  getClassSet,
+  prefix,
+  splitBsProps
+} from './utils/bootstrapUtils';
+import createChainedFunction from './utils/createChainedFunction';
+import ValidComponentChildren from './utils/ValidComponentChildren'; */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from "jquery";
+import Popper from "popper.js";
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { Nav,
+        NavItem,
+        NavDropdown,
+        MenuItem} from "react-bootstrap";
+class Navigation extends Component {
+    handleSelect(eventKey, event) {
+        event.preventDefault();
+        alert(`selected ${eventKey}`);
+    }
+    render() {
+        return (
+            <Nav bsStyle="tabs" activeKey="1" onSelect={k=>this.handleSelect(k)}>
+                <NavItem eventKey="1" href="/home">
+                NavItem 1 content
+                </NavItem>
+                <NavItem eventKey="2" href="Item">
+                NavItem 2 content
+                </NavItem>
+                <NavItem eventKey="3" disabled>
+                NavItem  disabled
+                </NavItem>
+            <NavDropdown eventKey="4" title="Dropdown" id="nav-dropdown">
+                <MenuItem eventKey="4.1">Action</MenuItem>
+                <MenuItem eventKey="4.2">Another action</MenuItem>
+                <MenuItem eventKey="4.3">Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="4.4">Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+    );
+  }
+}
+
+export default Navigation ;
