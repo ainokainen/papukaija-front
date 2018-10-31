946234b6522409f72517b5ff407d59a413f11798
ainokainen <aino.rouvinen@gmail.com>
2018-10-31 09:41:54 +0200

    Navigaatio lisätty

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import TehtavaBox from "./components/TehtavaBox";
+import Navigation from "./components/Navigation";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <Navigation/>
           <TehtavaBox/>
         </header>
       </div>
