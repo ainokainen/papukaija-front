32fe560b393e1428a8414295443b3d12880e3ea3
marjajohannaK <marjajkurkela@gmail.com>
2018-11-01 16:10:19 +0200

    muotoiluja

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -27,48 +27,20 @@ class Navigation extends Component {
                 <NavItem className="navItems" e eventKey="3" href="/done">
                 <br/>Tehdyt<br/><br/><br/>
                 </NavItem>
-                
-            <NavDropdown className="navItems" eventKey="4" title="Profiili" id="nav-dropdown">
-                <br/><br/>
-                <MenuItem eventKey="4.1">Action</MenuItem>
-                <MenuItem eventKey="4.2">Another action</MenuItem>
-                <MenuItem eventKey="4.3">Something else here</MenuItem>
-                <MenuItem divider />
-                <MenuItem eventKey="4.4">Separated link</MenuItem>
-            </NavDropdown>
+                <NavItem>
+                    <Media>
+                        <Media.Left align="bottom">
+                            <img className="image" src="https://trello-attachments.s3.amazonaws.com/5bd715c372be28202555c3ef/5bd80baf68c39a065a05d18e/b2966e72bd3b5aeb14570f8a36b8e044/Gold.png" alt="parrot"responsive/>
+                        </Media.Left>
+                    </Media>
+                </NavItem>
         </Nav>
         </div>
-        <div className="panelwidth">
-        <Panel id="omapanel" collapsible defaultExpanded>
-            <Panel.Heading className="paneeliheading">
-            <input className="bDone" type="button" value="Done!"/>
-            <input className="bPoista" type="button" value="Poista!"/>
-            <Panel.Title toggle>Tekemätön tehtävä otsikko </Panel.Title>
-            </Panel.Heading>
-           <Panel.Collapse>
-                <Panel.Body>
-                Tänne tehtävän tehtävän kuvaus ja informaatiota ja olisko meillä jotain muuta sälää tänne?
-                </Panel.Body>
-            </Panel.Collapse>
-        </Panel>
-        </div>
+        
          <div className="panelwidth">
         <div>
-        <Media>
-        <Media.Left align="bottom">
-      <img className="image" src="https://trello-attachments.s3.amazonaws.com/5bd715c372be28202555c3ef/5bd80baf68c39a065a05d18e/b2966e72bd3b5aeb14570f8a36b8e044/Gold.png" alt="parrot"responsive/>
-    </Media.Left>
-    <Media.Body>
-      <Media.Heading>Bottom aligned media</Media.Heading>
-      <p>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-        ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-        tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-        fringilla. Donec lacinia congue felis in faucibus.
-      </p>
-    </Media.Body>
-  </Media>
-  </div>
+       
+        </div>
         </div> 
         </div>
         );
--- src/components/Tehtava.js
+++ src/components/Tehtava.js
@@ -6,13 +6,13 @@ class Tehtava extends Component {
             <div>
                Tehtävän nimi:<br/> {this.props.tehtava.Nimi}<br/>
                 Kuvaus: <br/> {this.props.tehtava.Kuvaus}<br/><br/>
-                Tehty: {(this.props.tehtava.Tehty).toString()}<br/>
+                {/* Tehty: {(this.props.tehtava.Tehty).toString()}<br/> */}
                 {/* {this.props.tehtava.pisteet}<br/>
                 {this.props.tehtava.kuva}<br/>
                 {this.props.tehtava.aikaleima}<br/>
                 {this.props.tehtava.kategoria}<br/>
                 {this.props.tehtava.tehty}<br/> */}
-                <input type = "button" value = "Muokkaa" onClick= "" /> 
+
 
             </div>
         );
